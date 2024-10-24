4e20898442ed6113b5b8182f197693ab468e93af
akashkumarsky <akash220701@gmail.com>
2024-10-24 15:09:46 +0530

    Controller for Rating

---
+++ rating/src/main/java/com/sky/ratings/controllers/RatingController.java
@@ -0,0 +1,42 @@
+package com.sky.ratings.controllers;
+
+import com.sky.ratings.entities.Rating;
+import com.sky.ratings.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingController {
+
+    @Autowired
+    private RatingService ratingService;
+
+    // create rating
+    @PostMapping
+    public ResponseEntity<Rating> create(@RequestBody Rating rating){
+        return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
+    }
+
+    // get all rating
+    @GetMapping
+    public ResponseEntity<List<Rating>> getRatings(){
+        return ResponseEntity.ok(ratingService.getAllRatings());
+    }
+
+    // get all rating by userId
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<Rating>> getRatingByUserId(@PathVariable String userId){
+        return ResponseEntity.ok(ratingService.getAllRatingByUserId(userId));
+    }
+
+    // get all rating by hotelId
+    @GetMapping("/hotels/{hotelId}")
+    public ResponseEntity<List<Rating>> getRatingByHotelId(@PathVariable String hotelId){
+        return ResponseEntity.ok(ratingService.getAllRatingByHotelId(hotelId));
+    }
+}
