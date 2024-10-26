0219d510157707be8eee6d65c8708294d447f078
akashkumarsky <akash220701@gmail.com>
2024-10-26 12:54:01 +0530

    fatching Rating in getUser

--- userservice/src/main/java/com/sky/userservice/services/impl/UserServiceImpl.java
+++ userservice/src/main/java/com/sky/userservice/services/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.sky.userservice.services.impl;
 
+import com.sky.userservice.entities.Rating;
 import com.sky.userservice.entities.User;
 import com.sky.userservice.exceptions.ResourceNotFoundExcetion;
 import com.sky.userservice.repositories.UserRepository;
 import com.sky.userservice.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +21,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger  logger= LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -32,6 +42,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundExcetion("User with id is not found on server !! :"+ userId ));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundExcetion("User with id is not found on server !! :"+ userId ));
+
+        ArrayList<Rating> ratingforObject = restTemplate.getForObject("http://localhost:8091/ratings/users/"+user.getUserId(), ArrayList.class);
+        logger.info("{} ",ratingforObject);
+        user.setRatings(ratingforObject);
+        return user;
     }
 }
